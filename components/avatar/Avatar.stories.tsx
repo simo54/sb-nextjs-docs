6b0b28aa32af10e0bc39daa425161cee3ffb5e67
scava <sorzesimone1@gmail.com>
2021-11-01 13:48:52 +0100

    update name

--- components/avatar/Avatar.stories.tsx
+++ components/avatar/Avatar.stories.tsx
@@ -1,15 +1,13 @@
-// Button.stories.ts | Button.stories.tsx
-
 import { Meta } from "@storybook/react";
-
 import Avatar from "./Avatar";
+// import { decorators } from "../../.storybook/preview";
 
 export default {
   component: Avatar,
   title: "Components/Avatar",
 } as Meta;
 
-export const Primary: React.VFC<{}> = () => (
+export const BasicAvatar: React.VFC<{}> = () => (
   <Avatar
     src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
     alt="Colm Tuite"
