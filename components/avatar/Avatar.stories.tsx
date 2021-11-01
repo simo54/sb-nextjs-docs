b330e1a891e044e830fcb7c9ed48b616e17c421a
scava <sorzesimone1@gmail.com>
2021-11-01 15:51:12 +0100

    initial test with rendering first doc page

--- components/avatar/Avatar.stories.tsx
+++ components/avatar/Avatar.stories.tsx
@@ -1,15 +1,23 @@
 import { Meta } from "@storybook/react";
 import Avatar from "./Avatar";
-// import { decorators } from "../../.storybook/preview";
+import AvatarDoc from "./AvatarDoc";
 
 export default {
-  component: Avatar,
   title: "Components/Avatar",
+  component: Avatar,
+  parameters: {
+    layout: "centered",
+    docs: {
+      page: () => <AvatarDoc />,
+    },
+  },
 } as Meta;
 
-export const BasicAvatar: React.VFC<{}> = () => (
-  <Avatar
-    src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-    alt="Colm Tuite"
-  />
-);
+export const BasicAvatar: React.VFC<{}> = () => {
+  return (
+    <Avatar
+      src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+      alt="Colm Tuite"
+    />
+  );
+};
---
+++ components/avatar/AvatarDoc.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  Stories,
+} from "@storybook/addon-docs";
+
+export default function AvatarDoc() {
+  return (
+    <div style={{ margin: "100px" }}>
+      <Title>Avatar</Title>
+      <Subtitle />
+      <Description>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </Description>
+      <Primary />
+      <Stories title={"Test"} />
+    </div>
+  );
+}
